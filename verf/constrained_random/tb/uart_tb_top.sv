6f9576aca56c7892ff39cea12814250d7cba6309
SathsaraGeeth <geethwa03@gmail.com>
2026-03-26 16:34:55 +0000

    update uart_tb_top

---
+++ verf/constrained_random/tb/uart_tb_top.sv
@@ -0,0 +1,77 @@
+// import uvm pkg and macros
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+// import our pkg and macros
+
+module uart_tb_top;
+
+    // interface instance
+    uart_if vif();
+
+    // DUT instance
+    localparam RX_DEPTH = 8;
+    localparam TX_DEPTH = 8;
+    uart #(
+        .RX_DEPTH(RX_DEPTH),
+        .TX_DEPTH(TX_DEPTH),
+        .PARITY_EN(0),
+        .EN_2STOP_BITS(0)
+    ) uart_inst (
+        .i_clk(vif.clk),
+        .i_rst_n(vif.rst_n),
+
+        .i_baud_div(vif.baud_div),
+
+        .o_deq_rx_data(vif.deq_rx_data),
+        .o_deq_rx_ready(vif.deq_rx_ready),
+        .i_deq_rx_valid(vif.deq_rx_valid),
+
+        .o_rx_full(),
+        .o_rx_empty(),
+        .o_rx_level(),
+
+        .i_enq_tx_data(vif.enq_tx_data),
+        .i_enq_tx_valid(vif.enq_tx_valid),
+        .o_enq_tx_ready(vif.enq_tx_ready),
+
+        .o_tx_full(),
+        .o_tx_empty(),
+        .o_tx_level(),
+
+
+        .i_rx(vif.rx),
+        .o_tx(vif.tx)
+    );
+
+    // Initialize interface(bfm) and run test
+    initial begin
+        uvm_config_db #(virtual uart_if)::set(null, "*", "vif", vif);
+        run_test();
+    end
+
+    // Clock initialization
+    initial begin
+        // to prevent x propogation in sim
+        vif.rst_n        = 1'b0;     // start in reset
+        vif.rx           = 1'b1;     // idle high for UART
+        vif.baud_div     = 32'd868;  // example baud divider
+        vif.enq_tx_data  = 8'd0;
+        vif.enq_tx_valid = 1'b0;
+        vif.deq_rx_valid = 1'b0;
+        vif.clk = 1'b0;
+
+        repeat (5) @(posedge vif.clk);
+        vif.rst_n = 1'b1;  // release reset
+
+        // waveform
+        $dumpfile("uart_tb.vcd");
+        $dumpvars(0, uart_tb_top);
+    end
+
+    // Clock generator
+    always begin
+        #10 vif.clk = ~vif.clk;
+    end
+
+endmodule: uart_tb_top
