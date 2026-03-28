7518ac8c6aac533f4eaed29a8815af48ba78de85
SathsaraGeeth <geethwa03@gmail.com>
2026-03-28 18:19:13 +0000

    ckpt: uart_txn

--- verf/constrained_random/sequences/uart_txn.sv
+++ verf/constrained_random/sequences/uart_txn.sv
@@ -1,11 +1,7 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-typedef enum {UART_MON_RST, UART_MON_RX, UART_MON_TX} uart_mon_type_e;
-
-class uart_txn extends uvm_sequence_item;
-  uart_mon_type_e m_type;
-  
+class uart_txn extends uvm_sequence_item;  
   `uvm_object_utils_begin(uart_txn)
   `uvm_object_utils_end
   
@@ -14,23 +10,12 @@ class uart_txn extends uvm_sequence_item;
   endfunction
 endclass: uart_txn
 
-// class uart_rst_txn extends uart_txn;
+class uart_rst_txn extends uart_txn;
+  bit [31:0] baud_div;
 //   rand bit [31:0] baud_div;
 //   constraint c_baud_div {
 //     baud_div inside {[1:10000000]};
 //   }
-
-//   `uvm_object_utils_begin(uart_rst_txn)
-//     `uvm_field_int(baud_div, UVM_ALL_ON)
-//   `uvm_object_utils_end
-
-//   function new(string name = "uart_rst_txn");
-//     super.new(name);
-//   endfunction
-// endclass: uart_rst_txn
-
-class uart_rst_txn extends uart_txn;
-  bit [31:0] baud_div;
 
   `uvm_object_utils_begin(uart_rst_txn)
     `uvm_field_int(baud_div, UVM_ALL_ON)
@@ -38,16 +23,17 @@ class uart_rst_txn extends uart_txn;
 
   function new(string name = "uart_rst_txn");
     super.new(name);
-    // fixed baud_div
-    baud_div = 434;
+    baud_div = 16; // fixed baud_div
   endfunction
 endclass: uart_rst_txn
 
 class uart_tx_txn extends uart_txn;
   rand bit [7:0] data;
+  int unsigned   seq_number;
 
   `uvm_object_utils_begin(uart_tx_txn)
     `uvm_field_int(data, UVM_ALL_ON)
+    `uvm_field_int(seq_number, UVM_ALL_ON)
   `uvm_object_utils_end
 
   function new(string name = "uart_tx_txn");
@@ -57,10 +43,12 @@ endclass: uart_tx_txn
 
 
 class uart_rx_txn extends uart_txn;
-  bit [7:0] data;
+  bit [7:0]    data;
+  int unsigned seq_number;
 
   `uvm_object_utils_begin(uart_rx_txn)
     `uvm_field_int(data, UVM_ALL_ON)
+    `uvm_field_int(seq_number, UVM_ALL_ON)
   `uvm_object_utils_end
 
   function new(string name = "uart_rx_txn");
